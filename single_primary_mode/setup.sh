0e95f77f1c5c678950dc652dba9cd83730f7cc12
Val <regval@pm.me>
2023-03-13 16:01:03 +0800

    update script creating

--- single_primary_mode/setup.sh
+++ single_primary_mode/setup.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-sql_slave_user='CREATE USER "mydb_slave_user"@"%" IDENTIFIED BY "mydb_slave_pwd"; GRANT REPLICATION SLAVE ON *.* TO "mydb_slave_user"@"%"; FLUSH PRIVILEGES;'
-docker exec database_master sh -c "mysql -u root -pS3cret -e '$sql_slave_user'"
+# name config, rename if you run more than one group
+master_container="single_primary_mode_mysql-master_1"
+slave_first="single_primary_mode_mysql-slave1_1"
+slave_second="single_primary_mode_mysql-slave2_1"
 
-MS_STATUS=`docker exec database_master sh -c 'mysql -u root -pS3cret -e "SHOW MASTER STATUS"'`
-CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
-CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
-sql_set_master="CHANGE MASTER TO MASTER_HOST='database_master',MASTER_USER='mydb_slave_user',MASTER_PASSWORD='mydb_slave_pwd',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
-start_slave_cmd='mysql -u root -pS3cret -e "'
-start_slave_cmd+="$sql_set_master"
-start_slave_cmd+='"'
+# check master is available
+check_availability_request='select 1'
+master_status=`docker exec $master_container sh -c "mysql -u root -ppass -s -e '$check_availability_request'"`
+if [ "master_status" != "1" ]; then
+    echo "Master is not available"
+    exit 1
+fi
 
-docker exec database_slave sh -c "$start_slave_cmd"
-docker exec database_slave sh -c "mysql -u root -pS3cret -e 'SHOW SLAVE STATUS \G'"
+# create user for slave in master
+create_slave_request='CREATE USER "slave_user"@"%" IDENTIFIED BY "slave_pass"; GRANT REPLICATION SLAVE ON *.* TO "slave_user"@"%"; FLUSH PRIVILEGES;'
+
+docker exec $master_container sh -c "mysql -u root -ppass -e '$create_slave_request '"
+
+check_privilege_request='select User, Repl_slave_priv from mysql.user where User="slave_user"'
+docker exec $master_container sh -c "mysql -u root -ppass -e '$check_privilege_request '"
+
+# get master status
+MS_STATUS=`docker exec $master_container sh -c 'mysql -u root -ppass -e "SHOW MASTER STATUS"'`
+CURRENT_LOG=`echo $MS_STATUS | awk 'NR==2{print $1}'`
+CURRENT_POS=`echo $MS_STATUS | awk 'NR==2{print $2}'`
+
+# set slave
+sql_set_master="CHANGE MASTER TO MASTER_HOST='mysql-master',MASTER_USER='slave_user',MASTER_PASSWORD='slave_pass',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
+docker exec $slave_first sh -c "mysql -u root -ppass -e \"$sql_set_master\""
+
+docker exec $slave_first sh -c "mysql -u root -ppass -e 'SHOW SLAVE STATUS \G'"
